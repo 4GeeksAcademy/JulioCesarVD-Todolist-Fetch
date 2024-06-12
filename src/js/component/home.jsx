488b562f7c3f39515c16571e5d37dfe031c27e78
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-12 08:51:40 +0000

    renderizacion del componente todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,33 @@
 import React from "react";
-import "../../styles/index.css"; 
-
+import TodoList from "./Todolist";
 import backgroundImage from "../../img/pexels-googledeepmind-17483850.jpg";
 
 const Home = () => {
-    return (
-        <div className="container-fluid text-center">
-            {/* Fondo de pantalla */}
-            <div
-                className="background-image"
-                style={{ backgroundImage: `url(${backgroundImage})` }}
-            ></div>
+  return (
+    <div className="container-fluid text-center">
+      {}
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
 
-            <div className="content">
-                <p className="mt-4">
-                    Bienvenido a la aplicación de Todolist de 4Geeks Academy.
-                    ¡Empieza a organizar tus tareas hoy mismo!
-                </p>
-                <p className="mt-4">
-                    Hecho por{" "}
-                    <a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-                    con amor 💚
-                </p>
-            </div>
-        </div>
-    );
+      <div className="content">
+        <p className="mt-4">
+          Bienvenido a la aplicación de Todolist de 4Geeks Academy.
+          ¡Empieza a organizar tus tareas hoy mismo!
+        </p>
+        <p className="mt-4">
+          Hecho por{" "}
+          <a href="https://github.com/JulioCesarVD">Github: JulioCesarVD</a>,
+          con amor 💚
+        </p>
+     
+      </div>
+      {}
+      <TodoList />
+    </div>
+
+  );
 };
 
 export default Home;
