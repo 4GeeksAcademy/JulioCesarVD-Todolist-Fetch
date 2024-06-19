96cd091aa68792a5951fa19d4401364b194edcc9
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-19 11:12:48 +0000

    any changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./component/home";
-import  "../styles/index.css"
+    import React from "react";
+    import ReactDOM from "react-dom";
+    import Home from "./component/home";
+    import  "../styles/index.css"
 
-ReactDOM.render(<Home />, document.querySelector("#app"));
+    ReactDOM.render(<Home />, document.querySelector("#app"));
